9705bee2d6c0be4c59834086392e2dd8ef9bb576
vishakha-shete <vishakhashete2@gmail.com>
2025-11-08 22:37:49 +0530

    practice questions of function in js

--- day-5/script.js
+++ day-5/script.js
@@ -182,6 +182,146 @@ let multiply = (a,b)=>{
 };
 
 
+//predict the output of the code
+
+function sayhi (name= "guest"){
+    console.log("hi",name);
+}
+sayhi();
+
+
+//what does the ... parameter in operator
+function vishu(...val){
+    console.log(val);
+}
+vishu(1,2,3,4,5,6,7,8,9);
+
+function laby(a,b,c, ...val){
+    console.log(a,b,c,val);
+}
+laby(1,2,3,4,5,6,7,8,9);
+
+//use rest parameter to accept any number of scores and return the total
+
+function getScore(...scores){
+    let total = 0;
+    scores.forEach(function(val){
+        total = total + val;
+
+    });
+    return total;
+
+}
+console.log(getScore(39,40,50,60,70));
+
+//fix the function using early return
+
+// function checkAge(age){
+//     if(age<18){
+//         console.log("to young");
+//     }else{
+//         console.log("Allowed");
+//     }
+// }
+
+
+function checkAge(age){
+    if(age<18) return "to young";
+    return "Allowed";
+}
+console.log(checkAge(23));
+
+//whats the return value of this function
+
+function f(){
+    return;
+}
+console.log(f());
+//undefined
+
+//what does it mean when we say "functions are first-class citizens"?
+// -----function can treated as a variable
+
+
+//can you assign a function to a variable and then call it?show how.
+// ----ex
+
+let h = function(){
+    console.log("parth");
+
+}
+a();
+
+
+//pass a function into another function and execute it inside.
+
+function lara(val){
+val();
+}
+lara(function(){
+    console.log("vishuu");
+});
+
+
+//what is higher order function
+//----those function which return the function in it and accept the function along with'
+
+
+// function lop(){
+//     return function(){
+
+//     }
+ 
+// }
+
+
+// function val (val){
+
+// }
+// val(function(){
+
+// })
 
 
 
+//convert the above function into the pure function
+
+let total = 0;
+function base(num){
+    let newtotal = total;
+    newtotal += num; 
+}
+
+//what is closure? when is it created?
+
+function bab(){
+    let val =0;
+    return function(){
+        console.log(val);
+    };
+}
+
+function outer(){
+    let count = 0 ;
+    return function (){
+        count++;
+        console.log(count);
+    };
+}
+const counter = outer();
+counter();
+counter();
+
+//what is the use of IIFE? Name one real-World use case
+
+(function(){
+    console.log("vaishnavi");
+})();
+
+
+
+// what is a BMI calculator
+
+function bmi(){
+    
+}
