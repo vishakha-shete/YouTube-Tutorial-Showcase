a350a5006967ac7620ed255b539104489c294462
vishakha-shete <vishakhashete2@gmail.com>
2025-11-11 18:25:33 +0530

    lrarn more about arrays

--- day-7/script.js
+++ day-7/script.js
@@ -55,7 +55,7 @@ let splice = [20,30,40,50];
 splice .splice(2,3);
 console.log(splice);
 
-//SPLICE
+//SLICE
 //in slice we can remove the value from  of the array 
 //old ARRAY IS AS IT IS
 //SLICE CANNOT CHANGE OUR OLD ELEMENT
@@ -64,4 +64,54 @@ let newslice = slice .splice(0,3);
 console.log(slice);
 
 
+// REVERSE
+// in array reverse can arrange the array element in reverse the element
+//it changed your main array and reverse it
+let reverse = [10,20,30,40,50];
+reverse .reverse();
+console.log(reverse);
 
+
+//SORT
+//if we want to sort the array in decending or accending order we sort 
+//sort accept the function within ourself
+
+//decending order
+let sort = [1,4,2,6,3,5,7,10,8,9,];
+let newsort = sort.sort(function (a,b){
+    return b - a;
+
+})
+console.log(newsort);
+
+//accending ordeer
+let sort1 = [1,4,2,6,3,5,7,10,8,9,];
+let newsort1 = sort1.sort(function (a,b){
+    return a - b;
+
+})
+console.log(newsort1);
+
+
+//accending ordeer
+let sort2 = [20,10,30,50,40];
+let newsort2 = sort2.sort(function(v,s){
+    return v - s;
+})
+console.log(newsort2);
+
+//decending order
+let sort3 = [20,10,30,50,40];
+let newsort3 = sort3.sort(function(v,s){
+    return s - v;
+})
+console.log(newsort3);
+
+
+//let fruits = ["apple", "banana", "mango", "grapes"];
+// Task: change "banana" to "orange"
+// and print the new array
+
+let fruits = ["apple", "banana", "mango", "grapes"];
+fruits [1] = "orange";
+console.log(fruits);
