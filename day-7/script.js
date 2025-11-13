a278fab00b4dcc0365e9a311a066ed65f6ee1f8c
vishakha-shete <vishakhashete2@gmail.com>
2025-11-13 21:58:11 +0530

    solved some que on reduce

--- day-7/script.js
+++ day-7/script.js
@@ -185,7 +185,6 @@ console.log("the sum of number is:",sum);
 
 
 
-
 //now we are going move forward to map
 // MAP
 //map is only used when u wanted to make new array on the baes of previous array
@@ -314,16 +313,125 @@ console.log(newfruits3);
 
 
 
+//now going to learn the filter in javascript
+//filter is similar to the map or foreach
+
+//que.1)
+
+let filter = [10,20,30,40,50,60,70];
+let newfilter = filter.filter(function(val){
+    if (val<20) return true;
+});
+console.log(newfilter);
+
+
+//que.2)
+
+let filter1 = [10,20,30,40,50,60,70];
+let newfilter1 = filter1.filter(function(val){
+    return val>10;
+});
+console.log(newfilter1);
+
+
+//que.3)Keep only numbers greater than 50
+let nums1 = [20, 55, 40, 75, 10, 100];
+let newnums1 = nums1.filter(function(val){
+    return val >50;
+})
+console.log(newnums1);
+// Output → [55, 75, 100]
 
 
 
+//que.4)Keep only even numbers
+let arr7 = [1, 2, 3, 4, 5, 6];
+let newarr7 = arr7.filter(function(val){
+    return val % 2 ===0;
+})
+console.log(newarr7);
+// Output → [2, 4, 6]
+
+
+//que.5)Keep only names that start with “A”
+let names1 = ["Aarav", "Vishakha", "Aniket", "Pallu", "Tanmay"];
+let newnames1 = names1.filter(function(val){
+    return val[0]== "A";
+})
+console.log(newnames1);
+// Output → ["Aarav", "Aniket"]
+
+
+//que.6)Keep only numbers less than or equal to 30
+let values = [10, 25, 35, 40, 30];
+let newvalues = values.filter(function(val){
+    return val <= 30;
+
+})
+console.log(newvalues);
+// Output → [10, 25, 30]
+
+
+//que.7)Keep only words with length greater than 4
+let words = ["cat", "apple", "sun", "planet", "sky"];
+let newwords = words.filter(function(val){
+    return val.length > 4;
+})
+console.log(newwords);
+// Output → ["apple", "planet"]
+
+
+//now i am going to learn about reduce
+//it reduces the long value into one value
+// reduce() is used to combine all elements of an array into a single value (like sum, product, or even a new array/object).
+// accumulator → stores the result after each step
+// currentValue → current item of the array
+// initialValue → optional starting value (default: first element)
+
+// que.1)
+
+let acc = [10,20,30,40,50,60];
+let newacc = acc.reduce(function(acc,val){
+    return acc +val;
+},0);
+console.log(newacc);
+// Output → 210
 
 
 
+// que.2)
+
+let acc1 = [10, 20, 30, 40];
+let newacc1 = acc1.reduce(function(acc,val){
+    return acc+val;
+},0);
+console.log(newacc1);
+// Output → 100
+
+// que.3)Find the sum of all numbers in the array.
+
+let nums2 = [5, 10, 15, 20];
+let newnums2 = nums2.reduce(function(acc,val){
+    return acc+val;
+},0);
+console.log(newnums2);
+// Output → 50
+
+// que.4)Find the product of all numbers in the array.
+let numbers2 = [2, 3, 4];
+let newnumbers2 = numbers2.reduce(function(acc,val){
+    return acc*(val);
+},1);
+console.log(newnumbers2);
 
 
+// que.5)Find the maximum number in the array using reduce().
+let marks3 = [60, 85, 40, 90, 75];
+let newmarks3 = marks3.reduce(function(acc,val){
+    return acc > val ? acc : val;
 
-
+},40)
+console.log(newmarks3);
 
 
 
