c1dec6a8a6e75e32743181deddb9b612a19f39d4
vishakha-shete <vishakhashete2@gmail.com>
2025-11-17 23:51:24 +0530

    trying to add like symbol

--- dom-project-2/script.js
+++ dom-project-2/script.js
@@ -3,7 +3,11 @@ var con = document.querySelector("#container")
 var like = document.querySelector("i")
 
 con.addEventListener("dblclick",function(){
-           console.log("hello");
+    love.style.transform = 'trandlate (-50%,-50%) scale(1)'
 
 })
 
+setTimeout(function(){
+    
+
+},2000)
